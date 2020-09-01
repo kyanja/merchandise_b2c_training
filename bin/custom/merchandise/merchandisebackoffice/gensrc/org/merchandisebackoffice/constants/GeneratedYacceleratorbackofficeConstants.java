12a16c83e5a2c79a42aef497d2ba382b5403282b
akaoutharelidrissi <akaoutharelidrissi@sqli.com>
2020-09-01 16:15:54 +0100

    hybris 5 Developer Training Trails  19.05  II - Commerce: Customize Initialization Process_                                                           Impex - Basic Store Data_                                                           Impex - Create Products_                                                           Impex - Site CMS Content_

---
+++ bin/custom/merchandise/merchandisebackoffice/gensrc/org/merchandisebackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 1 sept. 2020 à 10:43:44                     ---
+ * ----------------------------------------------------------------
+ */
+package org.merchandisebackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "merchandisebackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
