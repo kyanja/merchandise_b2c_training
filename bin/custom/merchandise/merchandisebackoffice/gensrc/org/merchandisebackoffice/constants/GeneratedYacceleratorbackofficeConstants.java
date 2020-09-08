5d957102e24dc83926103a87b0801e20085aa82d
akaoutharelidrissi <akaoutharelidrissi@sqli.com>
2020-09-08 21:26:17 +0100

    tranining storefront hybris

---
+++ bin/custom/merchandise/merchandisebackoffice/gensrc/org/merchandisebackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 8 sept. 2020 à 19:23:34                     ---
+ * ----------------------------------------------------------------
+ */
+package org.merchandisebackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "merchandisebackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
